03cb7c1db720c327b9fc1e899e7e4e20b33877ef
ussvarma <93523488+ussvarma@users.noreply.github.com>
2022-04-08 21:01:45 +0530

    reinforcement learning

---
+++ Upper_confidence_bound.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Problem statement:
+#   Find out the best advertisement
+
+# ## Importing the libraries
+
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# # Upper Confidence Bound (UCB)
+
+# ## Importing the dataset
+
+# In[ ]:
+
+
+dataset = pd.read_csv('datasets/Ads_CTR_Optimisation.csv')
+dataset.head()
+
+
+# ## Implementing UCB
+
+# In[ ]:
+
+
+import math
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
+for n in range(0, N):
+  ad = 0
+  max_upper_bound = 0
+  for i in range(0, d):
+    if (numbers_of_selections[i] > 0):
+      average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+      delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+      upper_bound = average_reward + delta_i
+    else:
+      upper_bound = 1e400
+    if (upper_bound > max_upper_bound):
+      max_upper_bound = upper_bound
+      ad = i
+  ads_selected.append(ad)
+  numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+  reward = dataset.values[n, ad]
+  sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+  total_reward = total_reward + reward
+
+
+# ## Visualising the results
+
+# In[ ]:
+
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+
+
+# ## Improving the above model
+
+# In[ ]:
+
+
+
+N = 700
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
+for n in range(0, N):
+  ad = 0
+  max_upper_bound = 0
+  for i in range(0, d):
+    if (numbers_of_selections[i] > 0):
+      average_reward = sums_of_rewards[i] / numbers_of_selections[i]
+      delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
+      upper_bound = average_reward + delta_i
+    else:
+      upper_bound = 1e400
+    if (upper_bound > max_upper_bound):
+      max_upper_bound = upper_bound
+      ad = i
+  ads_selected.append(ad)
+  numbers_of_selections[ad] = numbers_of_selections[ad] + 1
+  reward = dataset.values[n, ad]
+  sums_of_rewards[ad] = sums_of_rewards[ad] + reward
+  total_reward = total_reward + reward
+
+
+# In[ ]:
+
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+
+
+# # Observation:
+# # 1. After 700 samples itself ,we can find that ad 5 is the best one.
