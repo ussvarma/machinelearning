80afba81f7d4ea482c765a4da57caa12fda48267
ussvarma <saivarmauddaraju@gmail.com>
2022-04-11 07:08:52 +0530

    linear_model

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -18,15 +18,15 @@ import pandas as pd
 """## Importing the dataset"""
 
 dataset = pd.read_csv('datasets/Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
+X = dataset.iloc[:, 1].values
 y = dataset.iloc[:, -1].values
-
+X = X.reshape(-1, 1)
 """## Training the Linear Regression model on the whole dataset"""
 
 from sklearn.linear_model import LinearRegression
 
 lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+lin_reg.fit(X, y.ravel())
 
 """## Training the Polynomial Regression model on the whole dataset"""
 
@@ -35,7 +35,7 @@ from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree=4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+lin_reg_2.fit(X_poly, y.ravel())
 
 """## Visualising the Linear Regression results"""
 
@@ -74,4 +74,4 @@ lin_reg.predict([[6.5]])
 
 lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
 
-# observation: polynomial model predict better than mulitiple regression model
+# observation: polynomial model predict better than multiple regression model
